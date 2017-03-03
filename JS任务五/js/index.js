696fa12432f792bddfeb391f3b2adafc1475d1a0
Your Name <364049242@qq.com>
2017-03-03 19:33:06 +0800

    JS任务五

--- JS任务五/js/index.js
+++ JS任务五/js/index.js
@@ -1,18 +1,52 @@
 window.onload=function(){
+	var btn1=document.getElementById("bt1") /* 获取按钮 和输入框 和 ul 的id*/
+	var btn2=document.getElementById("bt2")
+	var btn3=document.getElementById("bt3")
+	var btn4=document.getElementById("bt4")
 	
 	var lists=document.getElementById("list")
 	var in1=document.getElementById("in1") ;
 	var input=document.getElementById("input")/*text*/
 	var res=document.getElementById("re")
-	in1.onclick=getnum
+	btn1.addEventListener('click',function(){   /*为按钮绑定事件*/
+   var tex=input.value
+		var lis=lists.childNodes[0]
+		var nli=document.createElement("li")
+		nli.innerHTML=tex
+		nli.style.height=tex+"px"
+		lists.insertBefore(nli,lis)
+  },false);
+  btn2.addEventListener('click',function(){
+   	var tex=input.value
+		var nli=document.createElement("li")
+		nli.innerHTML=tex
+		nli.style.height=tex+"px"
+		lists.appendChild(nli)
+  },false);
+  btn3.addEventListener('click',function(){
+   var lis=lists.childNodes[0]
+		lists.removeChild(lis)
+		alert(lis.innerHTML)
+  },false);
+  btn4.addEventListener('click',function(){
+   var lis=lists.lastChild
+		lists.removeChild(lis)
+		alert(lis.innerHTML)
+  },false);
+ 
+	in1.onclick=getnum   
 	function getnum(){
     var chilld=lists.getElementsByTagName("li").length
 	 if(chilld<=60){
 		var tex=input.value
+		if(tex<100){
 		var li=document.createElement("li")
 		li.innerHTML=tex
 		li.style.height=tex+"px"
-		lists.appendChild(li)
+		lists.appendChild(li)}
+		else{
+			alert("数字最大为100")
+		}
 	
 	}
 	else{
@@ -27,8 +61,7 @@ window.onload=function(){
 		 for(var i=0;i<chillds.length;i++){
 		 data.push(chillds[i].offsetHeight)
 		}
-		 }
-	
+		}
 	 resert(data)
 	 data.length=0;
 	}
@@ -42,30 +75,21 @@ function resert(datas){
                 for (var j=i;j<datas.length;j++)
                 {
                     if(datas[i]>datas[j]){
-		var temp;
+	    	var temp;
 		 temp=datas[j] 
 		 datas[j]=datas[i]
 		 datas[i]=temp;
                 }
             }
-	/*for( var i=0;i<datas.length;i++){
-		if(datas[i]>datas[i+1]){
-		var temp;
-		 temp=datas[i+1] 
-		 datas[i+1]=datas[i]
-		 datas[i]=temp;
-		
-		}
-		else{
-			datas[i]=datas[i]
-		}
-		*/
+	
 	}
-
-	for( var s=0;s<datas.length;s++){
-		alert(datas[s])
-	 li[s].style.height=datas[s]+"px"
+  for( var s=0;s<datas.length;s++){
+  	 li[s].style.height=datas[s]+"px"
 	 li[s].innerHTML=datas[s]
-	}
+  }
+	
 }
+
+
+	
 }
